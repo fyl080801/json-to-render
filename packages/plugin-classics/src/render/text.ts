2f20f8870f5237cbc671bde543237a7627f2c6f7
fengyuanliang3 <fengyuanliang2@jd.com>
2021-01-28 10:51:42 +0800

    chore: 预先添加 plugins

---
+++ packages/plugin-classics/src/render/text.ts
@@ -0,0 +1,15 @@
+const hook: FunctionHook = () => (field, next) => {
+  const textProp = Reflect.getOwnPropertyDescriptor(field, 'text')
+
+  if (!textProp) {
+    next(field)
+    return
+  }
+
+  field.props = field.props || {}
+  field.props.innerText = textProp.value
+
+  next(field)
+}
+
+export default hook
