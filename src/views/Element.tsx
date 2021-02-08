1b4f3a0dc7ed6e2d16fb7b61cfdf880539d30974
fengyuanliang3 <fengyuanliang2@jd.com>
2021-02-08 10:25:43 +0800

    chore: element示例

--- src/views/Element.tsx
+++ src/views/Element.tsx
@@ -2,45 +2,169 @@ import { defineComponent, reactive } from 'vue'
 
 export default defineComponent({
   setup() {
-    const data = reactive({})
+    const data = reactive({
+      checks: [],
+    })
+
+    const datasource = reactive({
+      raw: {
+        type: 'rawdata',
+        data: {
+          options: [
+            {
+              key: 0,
+              label: 'select 1',
+            },
+            {
+              key: 1,
+              label: 'select 2',
+            },
+          ],
+        },
+      },
+    })
+
+    const listeners = reactive([
+      {
+        watch: '$:model.selected',
+        actions: [{ handler: '@model.checks:[`select ${model.selected+1}`]' }],
+      },
+    ])
 
     const fields = reactive([
       {
         component: 'el-form',
-        props: { labelWidth: '120px' },
+        rowProps: {
+          gutter: 20,
+        },
+        props: {
+          labelWidth: '120px',
+        },
         children: [
           {
             component: 'el-input',
-            formProps: { label: 'input1' },
-            model: 'model.text1',
+            model: 'model.text',
+            colProps: { span: 24 },
+            formProps: { label: 'text1' },
           },
           {
             component: 'el-select',
-            formProps: { label: 'select1' },
-            model: 'model.select1',
+            model: 'model.selected',
+            colProps: { span: 12 },
+            formProps: { label: 'selected' },
+            children:
+              '$:raw.options.map(item=>({component:"el-option", props:{label:item.label, value:item.key}}))',
+          },
+          {
+            component: 'el-checkbox-group',
+            model: 'model.checks',
+            colProps: { span: 12 },
+            formProps: {
+              label: 'checks',
+            },
+            children:
+              '$:raw.options.map(item=>({component:"el-checkbox", props:{label:item.label}}))',
+          },
+          {
+            component: 'el-slider',
+            model: 'model.num',
+            colProps: { span: 24 },
+            formProps: { label: 'silder' },
+            props: { min: 1, max: 24 },
+          },
+          {
+            component: 'el-input',
+            model: 'model.text',
+            colProps: { span: '$:model.num' },
+            formProps: { label: 'silder' },
           },
         ],
       },
-      { component: 'p', text: '$:JSON.stringify(model)' },
+      {
+        component: 'p',
+        text: '$:JSON.stringify(model)',
+      },
     ])
 
-    const onSetup = ({ prerender }: any) => {
-      prerender(({ injectProxy, context }: any) => (field: any, next: any) => {
-        if (!field.formProps) {
+    const onSetup = ({ prerender, datasource }: any) => {
+      // 在组件外部套一个 el-form-item
+      prerender(
+        ({ injectProxy, context }: any) => (field: any, next: any) => {
+          if (!field.formProps) {
+            next(field)
+            return
+          }
+
+          const formProps = field.formProps
+
+          delete field.formProps
+
+          next(
+            injectProxy(
+              {
+                component: 'el-form-item',
+                props: formProps,
+                children: [field],
+              },
+              context
+            )
+          )
+        },
+        2
+      )
+
+      // 在组件内部套一个 el-row
+      prerender(() => (field: any, next: any) => {
+        if (!field.rowProps) {
           next(field)
           return
         }
 
-        const formProps = field.formProps
+        const rowProps = field.rowProps
 
-        delete field.formProps
+        delete field.rowProps
 
-        next(
-          injectProxy(
-            { component: 'el-form-item', props: formProps, children: [field] },
-            context
+        field.children = [
+          {
+            component: 'el-row',
+            props: rowProps,
+            children: field.children,
+          },
+        ]
+
+        next(field)
+      })
+
+      // 在组件外部套一个 el-col
+      prerender(
+        ({ injectProxy, context }: any) => (field: any, next: any) => {
+          if (!field.colProps) {
+            next(field)
+            return
+          }
+
+          const colProps = field.colProps.__jr_proxyDefine
+
+          delete field.colProps
+
+          next(
+            injectProxy(
+              {
+                component: 'el-col',
+                props: colProps,
+                children: [field],
+              },
+              context
+            )
           )
-        )
+        },
+        1
+      )
+
+      datasource('rawdata', ({ define }: any) => {
+        const { data } = define()
+
+        return data
       })
     }
 
@@ -49,6 +173,8 @@ export default defineComponent({
         <v-jrender
           v-model={data}
           fields={fields}
+          datasource={datasource}
+          listeners={listeners}
           class="j-form"
           onSetup={onSetup}
         ></v-jrender>
