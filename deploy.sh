f607303f298ba7999f27b7856c2e583293bfa32d
fengyuanliang3 <fengyuanliang2@jd.com>
2021-02-18 16:19:01 +0800

    docs: travis

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+yarn docs:build
+
+# mkdir docs/.vuepress/dist/example
+# cp -r example docs/.vuepress/dist
+
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'polish: docs'
+git push --force --quiet "https://${GITHUB_TOKEN}@${GITHUB_REF}" master:gh-pages
