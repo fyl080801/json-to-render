b6f79dfd6b82b750e6bcccc6ff687f3e1b1bde12
fengyuanliang3 <fengyuanliang2@jd.com>
2021-02-19 19:31:22 +0800

    docs: 完善文档

--- docs/.vitepress/configs/sidebar.js
+++ docs/.vitepress/configs/sidebar.js
@@ -5,6 +5,8 @@ module.exports = {
       children: [
         { text: '介绍', link: '/guide/' },
         { text: '快速上手', link: '/guide/getting-started' },
+        { text: '在线示例', link: '/guide/examples' },
+        { text: '仓库说明', link: '/guide/repo' },
       ],
     },
   ],
