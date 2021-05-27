7bc37618b17dbfd17b0e823f61c4eb6d985ba665
fengyuanliang3 <fengyuanliang2@jd.com>
2021-05-27 11:16:32 +0800

    chore: package

--- packages/plugin-elementui/src/prerender/colProps.ts
+++ packages/plugin-elementui/src/prerender/colProps.ts
@@ -1,25 +1,27 @@
 import { FunctionHook } from '@json2render/utils'
 
-const hook: FunctionHook = ({ injectProxy, context }) => (field, next) => {
-  if (!field.col) {
-    next(field)
-    return
-  }
+const hook: FunctionHook =
+  ({ injectProxy, context }) =>
+  (field, next) => {
+    if (!field.col) {
+      next(field)
+      return
+    }
 
-  const colProps = field.col
+    const colProps = field.col
 
-  delete field.col
+    delete field.col
 
-  next(
-    injectProxy(
-      {
-        component: 'el-col',
-        props: typeof colProps === 'number' ? { span: colProps } : colProps,
-        children: [field],
-      },
-      context
+    next(
+      injectProxy(
+        {
+          component: 'el-col',
+          props: typeof colProps === 'number' ? { span: colProps } : colProps,
+          children: [field],
+        },
+        context
+      )
     )
-  )
-}
+  }
 
 export default hook
